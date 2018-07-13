2a41115c26fcdc0a22a03f6f9280b129dab76335
James Stuckey <jstuckey@rutgers.edu>
2018-07-13 14:12:08 -0400

    updated onboard styling"

--- src/components/onboarding.js
+++ src/components/onboarding.js
@@ -3,9 +3,26 @@ import '../styles/onboarding.css';
 
 export default function Onboarding(props) {
     return (
-        <section className="onboard-section">
-            <h2>Welcome to movie surfer!</h2>
-            <p>Come live out all your dreams in MovieSurfer!</p>
+        <section className="onboard-container">
+            <div className="onboard-section">
+                <h2>Welcome to Movie surfer!</h2>
+                <p className="intro-p">
+                    Love movies, TV or video games? Then you're in the right place!
+                </p>
+                <p className="intro-p">
+                    Love listifying things? You're in the right place!
+                </p>
+                <p className="intro-p">
+                    Just love the internet? You're in the right place!
+                </p>
+
+                <h3 className="onboard-stories-header">With Movie Surfer you can:</h3>
+                <ul className="onboard-ul">
+                    <li>Create lists of movies, TV shows or videos games</li>
+                    <li>Share the list with friends</li>
+                    <li>Sort through and rank your lists.</li>
+                </ul>
+            </div>
         </section>
     );
 }
