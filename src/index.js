10b96ae80b852c8e3f93a65f9005b6c92eb648f6
James Stuckey <jstuckey@rutgers.edu>
2018-09-26 13:10:32 -0400

    normalized css

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import { store } from './store';
 
+import 'normalize.css';
+import './index.css';
+
 ReactDOM.render(
     <Provider store={store}>
         <App />
