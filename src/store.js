01966c3b8b098bb2a004a79e99a8d8e4796914da
James Stuckey <jstuckey@rutgers.edu>
2018-10-08 18:07:08 -0400

    removed useless protectedDAta reducer and remnants

--- src/store.js
+++ src/store.js
@@ -3,7 +3,6 @@ import thunk from 'redux-thunk';
 import { moviesReducer } from './reducers/movies';
 import { authReducer } from './reducers/users';
 import { reducer as formReducer } from 'redux-form';
-import { protectedDataReducer } from './reducers/protected-data';
 import { loadAuthToken } from './local-storage';
 import { setAuthToken, refreshAuthToken } from './actions/auth';
 
@@ -11,8 +10,7 @@ export const store = createStore(
     combineReducers({
         movies: moviesReducer,
         form: formReducer,
-        auth: authReducer,
-        listData: protectedDataReducer
+        auth: authReducer
     }),
     // compose(
     applyMiddleware(thunk)
