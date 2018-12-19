04d02672faf4bf74d217b6385c703b94fa6ad828
James Stuckey <jstuckey@rutgers.edu>
2018-12-19 14:00:49 -0500

    fixed api base url

--- src/store.js
+++ src/store.js
@@ -7,20 +7,18 @@ import { loadAuthToken } from './local-storage';
 import { setAuthToken, refreshAuthToken } from './actions/auth';
 
 export const store = createStore(
-    combineReducers({
-        movies: moviesReducer,
-        form: formReducer,
-        auth: authReducer
-    }),
-    // compose(
-    applyMiddleware(thunk)
-    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    // )
+	combineReducers({
+		movies: moviesReducer,
+		form: formReducer,
+		auth: authReducer
+	}),
+
+	applyMiddleware(thunk)
 );
 
 const authToken = loadAuthToken();
 if (authToken) {
-    const token = authToken;
-    store.dispatch(setAuthToken(token));
-    store.dispatch(refreshAuthToken());
+	const token = authToken;
+	store.dispatch(setAuthToken(token));
+	store.dispatch(refreshAuthToken());
 }
